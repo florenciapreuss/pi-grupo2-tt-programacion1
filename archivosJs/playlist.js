a41e8fa7aefeb86663d18ea7dda0b29f14d974d2
ValentinaQuispe12 <vquispe@udesa.edu.ar>
2023-06-04 23:22:01 -0300

    hice playlist.html, hice playlist.js, modifique detalle de canción y modifique css

--- archivosJs/playlist.js
+++ archivosJs/playlist.js
@@ -1,1 +1,20 @@
-// comment
+let cancionesFavoritas = localStorage.getItem('pepito') // Traigo los datos del localStorage con getItem 
+let arrayplaylist = JSON.parse(cancionesFavoritas)  // Los transformo en un array // Parsee cancionesFavoritas
+let imgplaylist = document.querySelector('.imgplaylist')    // Declare la variables imgplaylist para capturar el elemento en donde quiero que se muestren las fotos 
+let tituloplaylist = document.querySelector('.titulovalita')
+if(arrayplaylist.length == 0){ // Lo que le indique a este bloque fue basicamente que si el array cancionesFavoritas esta vacio, que diga:
+    tituloplaylist.innerHTML = 'No agregaste ninguna cancion a tu playlist'
+} else{ // Hice fetch para que muestre lo que el usuario puso en la playlist
+    for(let i=0 ; i < arrayplaylist.length; i++){
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${arrayplaylist[i]}`)        
+                                                                    // Interpolo el arrayPlaylist, que es en donde esta la informacion. 
+        .then(function(respuesta){
+            return respuesta.json()
+        })
+        .then(function(track){
+         imgplaylist.innerHTML += `<a class="links" href="detallecancion.html?id=${track.id}"><img src="${track.album.cover_xl}" alt="Album${track.album.title}">${track.title}</img></a>`
+
+        })
+        .catch(function(error){
+            console.log(error);
+        })}}        
