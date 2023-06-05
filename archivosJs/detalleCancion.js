04488341cf8b79732e91228c0d8c28fb5890cb56
Antonella Castellani <acastellani@udesa.edu.ar>
2023-06-04 21:05:24 -0300

    Modifiqué el archivo generos.html y generos.js

--- archivosJs/detalleCancion.js
+++ archivosJs/detalleCancion.js
@@ -21,3 +21,22 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cancio
         detalleCancion.innerHTML = html;
     })
     .catch(error => console.log(error));
+
+  
+  
+/* Cantidad de letras en busqueda: */
+let form = document.querySelector('.form');
+let input = document.querySelector('.busqueda');
+
+form.addEventListener('submit', function(e) {
+   e.preventDefault()
+
+
+   if (input.value.length >= 3) {
+      form.submit();
+   } else if (0 < input.value.length < 3) {
+    alert("Debes escribir por lo menos tres caracteres");
+   } else {
+       alert("El campo de búsqueda está vacío");
+   }
+});
