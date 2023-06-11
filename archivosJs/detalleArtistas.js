f0f9d9bbd201d503d2d8a2615194c7e29c8d88e9
florenciapreuss <fpreuss@udesa.edu.ar>
2023-06-11 18:38:59 -0300

    Edite detalleArtistas para que aparezcan los top 5 albumes

--- archivosJs/detalleArtistas.js
+++ archivosJs/detalleArtistas.js
@@ -35,7 +35,32 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${artis
         artistTitle.innerText = data.name;
         image.src             = data.picture_medium;
         nb_album.innerText    = data.nb_album;
-        console.log(data);
+       
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${artistId}/albums`)
+            .then(function (respuesta) {
+                return respuesta.json();
+            })
+            .then(function (data) {
+                let album = data.data
+                let listAlbums = document.querySelector('.listAlbums')
+
+                for (let i = 0; i < 5; i++) {
+                    let fotoAlbm = album[i].cover_medium
+                    let nombreAlbm = album[i].title
+                    let idAlbm = album[i].id
+                    listAlbums.innerHTML += `
+            <article class="lista_albumes">
+            <img class="imagen" src="${fotoAlbm}" alt="${nombreAlbm}">
+            <h3 class="nombre_artista">
+                <a href="detail-album.html?id=${idAlbm}">${nombreAlbm}</a>
+            </h3>
+            </article>
+            `
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     })
     .catch(function(error) {
         console.log("Error: " + error);
