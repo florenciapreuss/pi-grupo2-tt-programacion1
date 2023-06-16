bfa5d2ced0777789ecdae94f621f7ba26cbde936
florenciapreuss <fpreuss@udesa.edu.ar>
2023-06-16 13:58:37 -0300

    cambios en el header y css

--- archivosJs/detalleCancion.js
+++ archivosJs/detalleCancion.js
@@ -37,7 +37,7 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cancio
 
 
         cancionTitle.innerText = data.title;
-        artist.innerText       = data.artist.name;
+        artist.innerText       += data.artist.name;
         album.innerText        += data.album.title;
         image.src              = data.album.cover_medium;
         image.alt              = data.album.title;
@@ -48,41 +48,33 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cancio
     })
 
 
-
-
-//Trabajo Local Storage -- valitaaaaaaa
-// capturamos elemento
-let agregarQuitar = document.getElementById('agregarQuitar')
-// creamos un array
+//PARA AGREGAR A LA PLAYLIST
+let agregarQuitar = document.getElementById('agregarQuitar');
 let miplaylist = [];
-// traemos los datos del local storage
-let traercanciones = localStorage.getItem('atrapar')    
-//console.log(traercanciones);
-//En las proximas lineas dice basicamente lo sgte: si los datos que traigo de "traercanciones" no son nulos, eso quiere decir que hay datos, por lo tanto, pido que se los traiga en forma de un array
+
+let traercanciones = localStorage.getItem('atrapar'); 
 if(traercanciones != null){
-    miplaylist = JSON.parse(traercanciones) //Estoy haciendo que dentro del array "miplaylist" el cual es un Array vacio,
-                                            //Ahora va a contener todos los datos en lo que seria el navegador a travez del local storage.
-                                            // me los trae en formato string y lo convierto en un Array
+    miplaylist = JSON.parse(traercanciones);
 }
-//verificando si el id esta o no en el Array
+
 if(miplaylist.includes(cancionId)){
-    agregarQuitar.innerText = 'Quitar de mi playlist'   //si en mi playlist ya esta la cancion, le digo que me cambie el titulo "anadir a favoritos" y que aparezca "quitar de favoritos"
+    agregarQuitar.innerText = 'Quitar de mi playlist'; 
 }
-// controlamos el evento click sobre el elemento capturado
+
 agregarQuitar.addEventListener('click', function(e){
-    e.preventDefault()                                    // si se hace click en el "agregar a mi playlist" (el cual esta dentro de la clase agregarQuitar)
-                                                         // le pedimos que agregue el elemento a el array de miplaylist
+    e.preventDefault();                
+                       
     if(miplaylist.includes(cancionId)){
-        let posicionCancion = miplaylist.indexOf(cancionId)
-        miplaylist.splice(posicionCancion,1)                
-        agregarQuitar.innerText = 'Agregar a mi playlist'
+        let posicionCancion = miplaylist.indexOf(cancionId);
+        miplaylist.splice(posicionCancion,1);           
+        agregarQuitar.innerText = 'Agregar a mi playlist';
     }else{
         miplaylist.push(cancionId)
-        agregarQuitar.innerText = 'Quitar de mi playlist'
+        agregarQuitar.innerText = 'Quitar de mi playlist';
     }
-    //cargamos los datos al local storage aplicandole el json.stringify a mi array
-    let cadenatexto = JSON.stringify(miplaylist)            // convierte un objeto  texto JSON, opcionalmente reemplaza valores si se indica una función de reemplazo, o si se especifican las propiedades mediante un array de reemplazo.
-    localStorage.setItem('atrapar',cadenatexto)
+
+    let cadenatexto = JSON.stringify(miplaylist);            
+    localStorage.setItem('atrapar',cadenatexto);
 })
 
 
